1d705c331ff08cb68b62575041879a041626b220
Hari <patel_hari12@yahoo.co.in>
2024-06-04 23:23:34 +1000

    - Updated active and inactive commands with role persistence - added mongodb

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const { exec } = require('child_process');
 require('dotenv').config();
 const commandHandler = require('./handlers/commandHandler');
 const eventHandler = require('./handlers/eventHandler');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 const client = new Client({
     intents: [
