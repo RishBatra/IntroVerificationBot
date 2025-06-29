49b0df144832595c388d07cd6a145eae2aa74a49
hariztek <patel_hari12@yahoo.co.in>
2025-06-30 00:23:11 +1000

    feat(intro): add 'archived' status to intro schema, allowing for better management of user intros

--- models/intro.js
+++ models/intro.js
@@ -8,7 +8,7 @@ const introSchema = new mongoose.Schema({
     status: { 
         type: String, 
         required: true, 
-        enum: ['pending', 'started', 'hold', 'denied', 'verified'],
+        enum: ['pending', 'started', 'hold', 'denied', 'verified', 'archived'],
         default: 'pending'
     },
     createdAt: { type: Date, default: Date.now },
